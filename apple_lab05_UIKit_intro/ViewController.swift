d0ec8cd63f023cdb923446e941be9a4827c3fe84
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2026-04-13 10:48:15 -0500

    Initial Commit

---
+++ apple_lab05_UIKit_intro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple_lab05_UIKit_intro
+//
+//  Created by Rafael Diego Chuco Yantas on 13/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
